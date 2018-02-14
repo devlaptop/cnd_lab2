725055e615b9b8e3933328f2d78a92968232f365
Shahed <devlaptop@icloud.com>
2018-02-14 20:48:52 +0000

    last commit 2

--- server.js
+++ server.js
@@ -12,9 +12,8 @@ function convertToCaps(str) {
 app.get('/', function(req,res) {
 
         console.log('Server got a request");
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.write('This is a test\n');
-      res.end();
+	
+          res.end('This is a test');
 });
 
 app.post('/api/capitalise', function(req, res) {
